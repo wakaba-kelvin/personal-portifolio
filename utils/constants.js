3cd9535549ffea653b80b3fca9b678f294ee5ce4
wakaba-kelvin <kimanik918@gmail.com>
2024-05-08 17:38:39 +0300

    final updates

--- utils/constants.js
+++ utils/constants.js
@@ -13,25 +13,25 @@ export const projects = [
       "Shoutout a Twitter user, and generate a custom profile card in under a minute. Update: Since Twitter announced new API pricing in 2023, this project no longer functions correctly, and has been archived.",
     stack: ["Html", "Tailwind", "React", "Next"],
     link: "http://yodlr.vercel.app",
-    repo: "https://github.com/danielcranney/yodlr",
+    // repo: "https://github.com/danielcranney/yodlr",
     isSiteLive: true,
   },
-  {
-    title: "Rate My Film",
-    overview:
-      "A single-page application that helps filmmakers learn more about who their film might be suitable for.",
-    stack: ["Html", "React", "Sass"],
-    link: "http://www.ratemyfilm.co.uk",
-    repo: "https://github.com/danielcranney/rate-my-film",
-    isSiteLive: true,
-  },
+  // {
+  //   title: "Rate My Film",
+  //   overview:
+  //     "A single-page application that helps filmmakers learn more about who their film might be suitable for.",
+  //   stack: ["Html", "React", "Sass"],
+  //   link: "http://www.ratemyfilm.co.uk",
+  //   repo: "https://github.com/danielcranney/rate-my-film",
+  //   isSiteLive: true,
+  // },
   {
     title: "Spotlight Media",
     overview:
       "The website for my corporate videography company. This features a contact form powered by NodeJs and SendGrid.",
     stack: ["Html", "ReactJs", "Next", "Node"],
     link: "http://www.wearespotlight.co.uk",
-    repo: "https://github.com/danielcranney/spotlight-media",
+    // repo: "https://github.com/danielcranney/spotlight-media",
     isSiteLive: true,
   },
   {
@@ -49,7 +49,7 @@ export const projects = [
       "A fully-responsive and quick-rendering image-based website for an ongoing academic project.",
     stack: ["Html", "React", "Next"],
     link: "http://www.gps-embroidery.com",
-    repo: "https://github.com/danielcranney/GPS-Embroidery",
+    // repo: "https://github.com/danielcranney/GPS-Embroidery",
     isSiteLive: true,
   },
 ];
